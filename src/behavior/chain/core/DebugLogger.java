ea5afa4dc4f0083041748cebadd806bb06f6530b
wind <liuxianzhie@qq.com>
2021-08-04 09:16:24 +0800

    feat(行为型模式): 责任链模式

---
+++ src/behavior/chain/core/DebugLogger.java
@@ -0,0 +1,16 @@
+package behavior.chain.core;
+
+/**
+ * @author wind
+ */
+public class DebugLogger extends AbstractLogger {
+
+    public DebugLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("debug-logger: " + message);
+    }
+}
---
+++ src/behavior/chain/core/InfoLogger.java
@@ -0,0 +1,16 @@
+package behavior.chain.core;
+
+/**
+ * @author wind
+ */
+public class InfoLogger extends AbstractLogger {
+
+    public InfoLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("info-logger: " + message);
+    }
+}
