2a8aadcaad5bad05340a746d0138e7669199358f
wind <liuxianzhie@qq.com>
2021-08-09 09:11:08 +0800

    feat(行为型模式): 状态模式

---
+++ src/com/wind/dp/behavior/state/core/Context.java
@@ -0,0 +1,17 @@
+package com.wind.dp.behavior.state.core;
+
+/**
+ * @author wind
+ */
+public class Context {
+
+    private State state;
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+}
